5d5f981f07c42a38d967465943c75a566ad65a56
artur-al <116903662+artur-al@users.noreply.github.com>
2025-03-11 18:15:44 -0300

    Atualização tbl Profissional

--- Tabelas/Artur Alves Munhoz Assis.sql
+++ Tabelas/Artur Alves Munhoz Assis.sql
@@ -1,20 +1,21 @@
-create database pi;
-use pi;
+create database AgroSense;
+use AgroSense;
 
 create table cliente(
 	idCliente int primary key auto_increment,
-    nome varchar(45),
-    email varchar(45),
+    nome_social varchar(45) not null,
+	nome_fantasia varchar(45),
+    email varchar(45) not null,
     telefone int,
-    cpf varchar(15),
-    senha varchar(45)
+    cnpj varchar(15) unique not null,
+    senha varchar(45) not null
 );
 create table sensor(
 	idSensor int primary key auto_increment,
     tipo varchar(45),
-    situacao tinyint
+    situacao tinyint not null
 );
 create table solo(
 	idMuda int primary key auto_increment,
-    umidade int
+    umidade int not null
 );
